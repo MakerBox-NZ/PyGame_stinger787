42c416a67c8eaba15466837fde2e226ed83b1b82
stinger787 <foo-stinger787@example.com>
2017-06-07 16:02:49 +1200

    fixed indentation

--- sheaGame.py
+++ sheaGame.py
@@ -35,16 +35,16 @@ backdropRect = screen.get_rect()
 # code runs many times
 
 while main == True:
-    for event.type == pygame.event.get():
+    for event in pygame.event.get():
         if event.type == pygame.KEYUP:
-        if event.key == ord('g'):
-        pygame.quit()
-        sys.exit()
-        main = False
+            if event.key == ord('g'):
+                pygame.quit()
+                sys.exit()
+                main = False
 
-screen.blit(backdrop, backdropRect)
+    screen.blit(backdrop, backdropRect)
 
-pygame.display.flip()
-clock.tick(fps)
+    pygame.display.flip()
+    clock.tick(fps)
 
 
